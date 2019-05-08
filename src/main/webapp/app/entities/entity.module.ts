be27db55352bbdf4e18369c225902cdd8972054e
JensXeonWorkstation <jenshoffmannatwork@gmail.com>
2019-05-08 21:03:33 +0200

    enabled websockets

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,38 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'region',
+        loadChildren: './region/region.module#JhipsterRabbitmqExampleRegionModule'
+      },
+      {
+        path: 'country',
+        loadChildren: './country/country.module#JhipsterRabbitmqExampleCountryModule'
+      },
+      {
+        path: 'location',
+        loadChildren: './location/location.module#JhipsterRabbitmqExampleLocationModule'
+      },
+      {
+        path: 'department',
+        loadChildren: './department/department.module#JhipsterRabbitmqExampleDepartmentModule'
+      },
+      {
+        path: 'task',
+        loadChildren: './task/task.module#JhipsterRabbitmqExampleTaskModule'
+      },
+      {
+        path: 'employee',
+        loadChildren: './employee/employee.module#JhipsterRabbitmqExampleEmployeeModule'
+      },
+      {
+        path: 'job',
+        loadChildren: './job/job.module#JhipsterRabbitmqExampleJobModule'
+      },
+      {
+        path: 'job-history',
+        loadChildren: './job-history/job-history.module#JhipsterRabbitmqExampleJobHistoryModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
